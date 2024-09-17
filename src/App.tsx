aeb68a85e5b323e731b74e04d5eecd52d882f3b0
giorgio324 <giorgikochuashvili2002@gmail.com>
2024-09-17 05:46:13 +0400

    fixed a routing bug for deployment

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<RootLayout />}>
       <Route index element={<Home />} />
-      <Route path='/create' element={<Create />} />
+      <Route path='create' element={<Create />} />
     </Route>
   )
 );
